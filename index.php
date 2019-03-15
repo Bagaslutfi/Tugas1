ad3a9ce776d472bdb17412c898e694cbaf9a9ec6
Bagaslutfi <48593442+Bagaslutfi@users.noreply.github.com>
2019-03-15 07:17:03 -0700

    Tugas1
    menampilkan kata HELLO WORLD

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Information Retrieval UNISBANK</title>
+		<style>
+			h1 {
+				color: black;
+				text-align: center;
+				margin-top: 10%;
+			}
+		</style>
+	</head>
+	<body>
+		<h1> <?php echo 'Hello World'; ?> </h1>
+	</body>
+</html>
